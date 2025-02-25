a3038a3705576d8c60124731606ec3cea438ee8f
Eluxifer <Eluxifer@users.noreply.github.com>
2025-02-25 03:58:45 +0000

    deploy: c84cd00f152e299dce70532c301d3da632aaab2c

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Best Smartphones of 2025 – Top Picks & Reviews",
-        "excerpt":"## 📱 Best Smartphones of 2025 – Our Top Picks The smartphone market in 2025 is **more advanced than ever**, featuring AI-powered cameras, ultra-fast processors, and sleek designs. After thorough testing, we’ve **picked the best smartphones*[...] ### 🔥 1. iPhone 16 Pro Max – **Best Overall** - **🔋 Battery**: 28-hour...","categories": ["smartphones","tech"],
+        "excerpt":"📱 Best Smartphones of 2025 – Our Top Picks The smartphone market in 2025 is more advanced than ever, featuring AI-powered cameras, ultra-fast processors, and sleek designs. After thorough testing, we’ve *picked the best smartphones[…] 🔥 1. iPhone 16 Pro Max – Best Overall 🔋 Battery: 28-hour life 📷 Camera:...","categories": ["smartphones","tech"],
         "tags": [],
         "url": "/techreviews/2025/02/24/best-smartphones-2025.html",
         "teaser": null
