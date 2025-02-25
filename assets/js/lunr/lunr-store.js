44495f38b68d98474a3ddda111afee6ccdbc5b88
Eluxifer <Eluxifer@users.noreply.github.com>
2025-02-25 21:51:27 +0000

    deploy: 27766aec36d0b4c41988189877f721b6727bce0f

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,12 +2,12 @@ var store = [{
         "title": "Best Smartphones of 2025 – Top Picks & Reviews",
         "excerpt":"📱 Best Smartphones of 2025 – Our Top Picks The smartphone market in 2025 is more advanced than ever, featuring AI-powered cameras, ultra-fast processors, and sleek designs. After thorough testing, we’ve *picked the best smartphones[…] 🔥 1. iPhone 16 Pro Max – Best Overall 🔋 Battery: 28-hour life 📷 Camera:...","categories": ["smartphones","tech"],
         "tags": [],
-        "url": "/techreviews/2025/02/24/best-smartphones-2025.html",
+        "url": "/2025/02/24/best-smartphones-2025.html",
         "teaser": null
       },{
         "title": "How Much RAM Do You Need in 2025?",
         "excerpt":"How Much RAM Do You Need in 2025? Hello tech enthusiasts, As technology continues to evolve at an unprecedented rate, so do the demands placed on our computing hardware. One of the most critical components that influences your computer’s performance is Random Access Memory (RAM). RAM is often one of...","categories": ["tech"],
         "tags": [],
-        "url": "/techreviews/2025/02/25/how-much-ram-you-need-in-2025.html",
+        "url": "/2025/02/25/how-much-ram-you-need-in-2025.html",
         "teaser": null
       }]
